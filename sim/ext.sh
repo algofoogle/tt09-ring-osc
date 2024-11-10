716856b1743a05ea58e013620ad71d899eba7520
Anton Maurovic <anton@foogle.com>
2024-11-10 13:16:03 +1030

    Workaround for 'extresist' crashing Magic [no_ci]

--- sim/ext.sh
+++ sim/ext.sh
@@ -3,9 +3,11 @@
 PROJECT_NAME=tt_um_algofoogle_tt09_ring_osc
 SRC_MAG_FILE=../runs/wokwi/final/mag/$PROJECT_NAME.mag
 
+rm *.ext *.sim *.nodes *.spice
+
 # Refresh source .mag file (from TT09 OL2 harden) if it exists:
 [ -f "$SRC_MAG_FILE" ] && cp "$SRC_MAG_FILE" .
 
 # Run extraction from .mag file to .spice:
-magic -noconsole -dnull tcl/extract_for_sim.tcl $PROJECT_NAME
+magic -noconsole -dnull spicex.tcl $PROJECT_NAME
 
--- sim/spicex.tcl
+++ sim/spicex.tcl
@@ -1,17 +1,20 @@
+# 1st argument is name of cell to extract (expected to resolve to $project.mag):
 set project [lindex $argv $argc-1]
 load $project.mag
-flatten tt_um_flat
-load tt_um_flat
+flatten MYFLAT
+load MYFLAT
 select top cell
 cellname delete $project
-cellname rename tt_um_flat ${project}_parax
+cellname rename MYFLAT ${project}_parax
 extract all
 ext2sim labels on
 ext2sim
 extresist tolerance 10
-extresist
+# extresist # Commented out because it seems to be crashing.
 ext2spice lvs
 ext2spice cthresh 0
 ext2spice extresist on
 ext2spice -o $project.sim.spice
 quit -noprompt
+# Output should be $project.sim.spice
+
